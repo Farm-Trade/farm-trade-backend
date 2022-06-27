9257dc80fe37cb56c4388c45a5bc81043468f01b
Oleh Skrypnyk <oleh.skrypnyk@bnymellon.com>
2022-06-27 20:31:49 +0300

    Initial commit

---
+++ src/main/java/com/farmtrade/FarmtradeApplication.java
@@ -0,0 +1,13 @@
+package com.farmtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmtradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmtradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmtrade/FarmtradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmtrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmtradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
