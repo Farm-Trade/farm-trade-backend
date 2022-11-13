12d48b839f6503469c2e21735b9d24e41546e2c0
Oleh Skrypnyk <oleh.skrypnyk@bnymellon.com>
2022-11-13 22:39:49 +0200

    Add default data

---
+++ src/main/resources/db/migration/V4__add_reseller_and_farmer_users.sql
@@ -0,0 +1,3 @@
+insert into users (id,activation_code,created_at,email,full_name,is_active,password,phone,role) values
+(9223372036854775806,null,'2018-10-20', 'farmer@gmail.com', 'farmer', true, 'farmtrade1764', '0667654647', 'FARMER'),
+(9223372036854775805,null,'2018-10-20', 'reseller@gmail.com', 'reseller', true, 'farmtrade1764', '0667654646', 'RESELLER');
