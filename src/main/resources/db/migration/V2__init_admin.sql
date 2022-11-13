8671b3510941dab2a57f19c56f14b78b22be61ff
Sazeracl <39574834+olehsov@users.noreply.github.com>
2022-11-13 22:22:34 +0200

    Revert "51 add migrations (#58)" (#59)
    This reverts commit 9eec6e05d2e35f29e70beafdfca528daa37a6d0c.

--- src/main/resources/db/migration/V2__init_admin.sql
+++
@@ -1,2 +0,0 @@
-insert into users (id,activation_code,created_at,email,full_name,is_active,password,phone,role)
-values (9223372036854775807,null,'2018-10-20', 'admin@gmail.com', 'admin', true, 'farmtrade1764', '0667654648', 'ADMIN');
